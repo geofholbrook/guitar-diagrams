6c65dc2c64ab8786feaf95c57d7c6e3d8ce093bc
Geoffrey Holbrook <geof@Geoffreys-MacBook-Pro.local>
2020-03-25 21:10:13 -0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import { GuitarChordDiagram } from '@musicenviro/ui-elements'
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const ref = React.createRef<HTMLCanvasElement>()
+
+	React.useEffect(() => {
+		const canvas = ref.current
+		if (!canvas) return
+		const ctx = canvas.getContext('2d')
+		if (!ctx) return
+
+		ctx.fillRect(0, 0, canvas.width, canvas.height)
+	})
+
+	return (
+		<div className="App">
+			<div style={{height: 300, width: 300}}>
+				<GuitarChordDiagram /> 
+			</div>
+
+
+
+			{/* <div>
+        <canvas height={'500'} ref={ref}></canvas>
+      </div> */}
+		</div>
+	);
 }
 
 export default App;
