e5a24b5da47d626765e98035d5da5e1d4052ff6c
Geoffrey Holbrook <geof@Geoffreys-MacBook-Pro.local>
2020-03-26 17:46:25 -0400

    Updates

---
+++ precache-manifest.f15e366dfb7ecca15fa974966e74502d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f856baa80259fa09dc812e31ff005eaf",
+    "url": "/guitar-diagrams/index.html"
+  },
+  {
+    "revision": "9b6c717409ef5abe05e8",
+    "url": "/guitar-diagrams/static/css/main.87128b37.chunk.css"
+  },
+  {
+    "revision": "0e7afa3cd5bb86b08eaf",
+    "url": "/guitar-diagrams/static/js/2.12b9edee.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/guitar-diagrams/static/js/2.12b9edee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b6c717409ef5abe05e8",
+    "url": "/guitar-diagrams/static/js/main.9b1ef200.chunk.js"
+  },
+  {
+    "revision": "95f13d994587ac2eb4a2",
+    "url": "/guitar-diagrams/static/js/runtime-main.6cc430bd.js"
+  }
+]);
